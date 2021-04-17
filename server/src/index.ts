504fee2457f9ed6995e222687dac36f4bffe31f9
Davi Fonseca <davi.brandao2013@gmail.com>
2021-04-17 15:29:59 -0300

    Backend is UP !

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,8 @@
-console.log("backend");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+console.log("Backend was started in port: 3333 ⚡");
+
+app.listen(3333);
