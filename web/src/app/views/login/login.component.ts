794f9740c0dce3ac3754b5e7e0f10bba657ccdf2
Maicon Souza <maiconsouzapa@gmail.com>
2021-04-22 00:35:25 -0300

    Página de login, formControl, estilização e estrutura

--- web/src/app/views/login/login.component.ts
+++ web/src/app/views/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,32 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+  private mode:string = 'login';
 
-  constructor() { }
+  login = {
+    username: new FormControl(''),
+    password: new FormControl('')
+  }
+
+  register = {
+    name: new FormControl(''),
+    lastname: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    discord_id: new FormControl(''),
+    github_id: new FormControl(''),
+    objective: new FormControl('')
+  }
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  handleLoginMode(mode:string) {
+    this.mode = mode
+  }
 }
