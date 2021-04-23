187ea79d325321af35ed04b3a4e8f6109212fa3a
Kesselyn <kesselynfortunato@hotmail.com>
2021-04-23 19:36:35 -0300

    Criado estrutura de pastas do microservice de Grupo

---
+++ server/src/group/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+// import { router } from "./routes/index";
+
+const app = express();
+app.use(express.json());
+
+// app.use("/", router);
+
+console.log("Groups was started in port: 4000 ⚡");
+
+app.listen(4000);
---
+++ server/src/group/src/routes/index.ts
@@ -0,0 +1,9 @@
+// import { Router } from "express";
+
+// import { groupRoutes } from "./group.routes";
+
+// const router = Router();
+
+// router.use("/group", groupRoutes);
+
+// export { router };
