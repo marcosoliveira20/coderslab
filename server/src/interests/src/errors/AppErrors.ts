996e9183a3e01dc8fbf18ba2c0575454269b6313
Kesselyn <kesselynfortunato@hotmail.com>
2021-04-24 22:56:09 -0300

    Criando arquitetura de pastas e arquivos de Interests

---
+++ server/src/interests/src/errors/AppErrors.ts
@@ -0,0 +1,12 @@
+export class AppError {
+  // Transformar em uma classe usando a lógica de message e status,
+  //  onde vamos preenhcer esses caras e quem chamou vai usa-los.
+  public readonly message: string;
+
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
