8c1a8b1ccf1166792b8564f103f26d7fffb7dae1
Maicon Souza <maiconsouzapa@gmail.com>
2021-04-25 15:50:10 -0300

    estrutura da pagina inicial e status do usuário

--- web/src/app/views/home/home.component.ts
+++ web/src/app/views/home/home.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // TODO integration
+  public user = {
+    name: "José",
+    lastname: "Bezerra",
+    username: "Flynn Rider",
+    email: "jose@email.com.br",
+    level: "Iniciante",
+    group_list: [
+      {
+        name: "Node para iniciantes",
+        user_list: 15,
+        schedule_list: [{datetime: new Date("2021-04-20")}, {datetime: new Date("2021-04-28")}],
+        category: [{label: "back end"}, {label: "dev web"}]
+      },
+      {
+        name: "MySQL básico",
+        user_list: 10,
+        schedule_list: [{datetime: new Date("2021-04-20")}, {datetime: new Date("2021-04-28")}],
+        category: [{label: "back end"}, {label: "dev web"}, {label: "dba"}]
+      },
+      {
+        name: "Lógica de programação",
+        user_list: [{}],
+        schedule_list: [{datetime: new Date("2021-04-20")}, {datetime: new Date("2021-04-28")}],
+        category: 35
+      }
+    ],
+    interest_list: [
+      {subject_label: "Lógica de programação", level: "", isDefault: true },
+      {subject_label: "Node", level: "", isDefault: false },
+    ]
+  }
+
   constructor() { }
 
   ngOnInit() {
