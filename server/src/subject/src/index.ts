4ffb311157111c7c7c9e4d527f15c65350f03ba0
Davi Fonseca <davi.brandao2013@gmail.com>
2021-04-25 00:24:30 -0300

    Rotas de Category funcionando

--- server/src/subject/src/index.ts
+++ server/src/subject/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
+import { categoryRoutes } from "./category.routes";
 import { subjectRoutes } from "./routes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/", subjectRoutes);
+app.use("/category", categoryRoutes);
 
 console.log("Subject was started in port: 5000 ⚡");
 
