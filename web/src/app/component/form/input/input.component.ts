74f0fe70629568c1eccdccd61a7b161d0d54a20f
Marcos de Oliveira Santos <marcossantos@ibm.com>
2021-04-24 21:34:49 -0300

    componente autocomplete funcional com dois temas

--- web/src/app/component/form/input/input.component.ts
+++ web/src/app/component/form/input/input.component.ts
@@ -1,14 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-input-auto-complete",
   templateUrl: "./input.component.html",
   styleUrls: ["./input.component.scss"],
 })
+
 export class BasicAutoCompleterComponent {
+  @Input() theme:string;
+
+
   keyword = "name";
   private interestList = [];
-
+  private interestListFilter =[]
   public interesses = [
     {
       id: 1,
@@ -37,7 +41,7 @@ export class BasicAutoCompleterComponent {
     {
       id: 7,
       name: "C#",
-    },
+    }
   ];
 
   selectEvent(item) {
@@ -52,9 +56,37 @@ export class BasicAutoCompleterComponent {
     var interestRemove = interest.path[0].className;
     const interestIndex = this.interestList.findIndex(
       (interest) => interest.name == interestRemove
-    );
+    ); 
     this.interesses.push(this.interestList[interestIndex]);
+    this.interesses = [...this.interesses.splice(interestIndex)]
+    
     this.interestList.splice(interestIndex, 1);
-    console.log(this.interestList);
+  }
+
+   addInterest(interest) {
+    var interestAdd = interest.path[0].className;
+    const interestIndex = this.interesses.findIndex(
+      (i) => i.name == interestAdd
+    );
+    this.interestList.push(this.interesses[interestIndex]);
+    this.interesses.splice(interestIndex, 1);
+    
+    this.interestListFilter.splice( this.interestListFilter.findIndex(
+      (i) => i.name == interestAdd
+    ), 1 );
+
+
+  }
+
+  filterList(event){
+    if(event.target.value.length >0){
+      this.interestListFilter = this.interesses.filter(interest => interest.name.toLowerCase().includes(event.target.value));
+    }else{
+      this.interestListFilter.splice(0);
+    }
+  }
+
+  reloadData() {
+    this.interestListFilter = [...this.interestListFilter];
   }
 }
