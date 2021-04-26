21c248390e077efdda58b78da117c85b718df762
Maicon Souza <maiconsouzapa@gmail.com>
2021-04-26 20:50:56 -0300

    nova view 'groups' e roteamento

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GroupsComponent } from './views/groups/groups.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
@@ -7,6 +8,7 @@ import { ProfileComponent } from './views/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'groups', component: GroupsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
   { path: '**', component: NotFoundComponent }
