b0a791e4ea28cedf764d8836ec0a773c502dbf08
Marcos Santos <marcos.oliveira129@hotmail.com>
2021-04-29 20:47:06 -0300

    trocando tipo do botão e concertando bug na lista de objetivos

--- web/src/app/component/form/input/input.component.ts
+++ web/src/app/component/form/input/input.component.ts
@@ -6,13 +6,12 @@ import { EventEmitter } from "events";
   templateUrl: "./input.component.html",
   styleUrls: ["./input.component.scss"],
 })
-
 export class BasicAutoCompleterComponent {
-  @Input() theme:string;
+  @Input() theme: string;
 
   keyword = "name";
   private interestList = [];
-  private interestListFilter =[]
+  private interestListFilter = [];
   public interestListMock = [
     {
       id: 1,
@@ -41,7 +40,7 @@ export class BasicAutoCompleterComponent {
     {
       id: 7,
       name: "C#",
-    }
+    },
   ];
 
   selectEvent(item) {
@@ -53,39 +52,43 @@ export class BasicAutoCompleterComponent {
   }
 
   removeInterest(interest) {
+    console.log(interest)
     var interestRemove = interest.path[0].className;
     const interestIndex = this.interestList.findIndex(
       (interest) => interest.name == interestRemove
-    ); 
+    );
     this.interestListMock.push(this.interestList[interestIndex]);
-    this.interestListMock = [...this.interestListMock.splice(interestIndex)]
-    
+    this.interestListMock = [...this.interestListMock.splice(interestIndex)];
+
     this.interestList.splice(interestIndex, 1);
   }
 
-   addInterest(interest) {
-    var interestAdd = interest.path[0].className;
-    const interestIndex = this.interestListMock.findIndex(
-      (i) => i.name == interestAdd
-    );
-    this.interestList.push(this.interestListMock[interestIndex]);
-    this.interestListMock.splice(interestIndex, 1);
-    
-    this.interestListFilter.splice( this.interestListFilter.findIndex(
-      (i) => i.name == interestAdd
-    ), 1 );
+  addInterest(interest) {
+    if (this.interestListFilter.length > 0) {
+      var interestAdd = interest.path[0].className;
+      const interestIndex = this.interestListMock.findIndex(
+        (i) => i.name == interestAdd
+      );
+      this.interestList.push(this.interestListMock[interestIndex]);
+      this.interestListMock.splice(interestIndex, 1);
 
-
+      this.interestListFilter.splice(
+        this.interestListFilter.findIndex((i) => i.name == interestAdd),
+        1
+      );
+    }
   }
 
-  filterList(event){
-    if(event.target.value.length >0){
-      this.interestListFilter = this.interestListMock.filter(interest => interest.name.toLowerCase().includes(event.target.value));
-    }else{
+  filterList(event) {
+    if (event.target.value.length > 0) {
+      this.interestListFilter = this.interestListMock.filter((interest) =>
+        interest.name.toLowerCase().includes(event.target.value)
+      );
+    } else {
       this.interestListFilter.splice(0);
     }
   }
-  public getInterestList(){
+  public getInterestList() {
     return this.interestList;
   }
 }
