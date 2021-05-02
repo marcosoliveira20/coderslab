dd5ccd9c13a02b3b478408d902c42f770ce44a6f
Marcos Santos <marcos.oliveira129@hotmail.com>
2021-05-01 23:16:00 -0300

    criação de explore  group

--- web/src/app/views/group/explore-group/explore-group.component.ts
+++ web/src/app/views/group/explore-group/explore-group.component.ts
@@ -1,15 +1,149 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-explore-group',
-  templateUrl: './explore-group.component.html',
-  styleUrls: ['./explore-group.component.scss']
+  selector: "app-explore-group",
+  templateUrl: "./explore-group.component.html",
+  styleUrls: ["./explore-group.component.scss"],
 })
 export class ExploreGroupComponent implements OnInit {
+  public user = {
+    name: "José",
+    lastname: "Bezerra",
+    username: "Flynn Rider",
+    email: "jose@email.com.br",
+    level: "Iniciante",
+    group_list: [
+      {
+        name: "Node para iniciantes",
+        user_list: 15,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [{ label: "back end" }, { label: "dev web" }],
+        isDefault: false,
+      },
+      {
+        name: "MySQL básico",
+        user_list: 10,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [
+          { label: "back end" },
+          { label: "dev web" },
+          { label: "dba" },
+        ],
+        isDefault: false,
+      },
+      {
+        name: "Lógica de programação",
+        user_list: 35,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [{ label: "back end" }, { label: "dev web" }],
+        isDefault: true,
+      },
+      {
+        name: "Node para iniciantes",
+        user_list: 15,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [{ label: "back end" }, { label: "dev web" }],
+        isDefault: false,
+      },
+      {
+        name: "MySQL básico",
+        user_list: 10,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [
+          { label: "back end" },
+          { label: "dev web" },
+          { label: "dba" },
+        ],
+        isDefault: false,
+      },
+      {
+        name: "Lógica de programação",
+        user_list: 35,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [{ label: "back end" }, { label: "dev web" }],
+        isDefault: true,
+      },
+      {
+        name: "Node para iniciantes",
+        user_list: 15,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [{ label: "back end" }, { label: "dev web" }],
+        isDefault: false,
+      },
+      {
+        name: "MySQL básico",
+        user_list: 10,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [
+          { label: "back end" },
+          { label: "dev web" },
+          { label: "dba" },
+        ],
+        isDefault: false,
+      },
+      {
+        name: "Lógica de programação",
+        user_list: 35,
+        level: "iniciante",
+        schedule_list: [
+          { datetime: new Date("2021-04-20") },
+          { datetime: new Date("2021-04-28") },
+        ],
+        category: [{ label: "back end" }, { label: "dev web" }],
+        isDefault: true,
+      },
+    ],
+    interest_list: [
+      { subject_label: "Lógica de programação", level: "", isDefault: true },
+      { subject_label: "Node", level: "", isDefault: false },
+    ],
+  };
 
-  constructor() { }
+  public objective_list = [
+    { id: 1, name: "Java" },
+    { id: 2, name: "CSS" },
+    { id: 3, name: "HTML" },
+  ];
 
-  ngOnInit() {
-  }
+  public category_list = [
+    { id: 1, name: "Backend" },
+    { id: 2, name: "Frontend" },
+    { id: 3, name: "Desing" },
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
