41d854479170f1024be86903763bdf80e4f8327f
Maicon Souza <maiconsouzapa@gmail.com>
2021-05-04 08:58:32 -0300

    objeto de mock global unificado e tratamento de label

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,6 +1,215 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+/**
+ * Mock object for tests before integration
+ */
+export const userMock = {
+  name: "José",
+  lastname: "Bezerra",
+  username: "Flynn Rider",
+  email: "jose@email.com.br",
+  level: "Iniciante",
+  group_list: [
+    {
+      token: 1,
+      name: "Node para iniciantes",
+      user_list: 15,
+      subject_label: "Lógica de programação",
+      level: 0,
+      schedule_list: [
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-04-20"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-05-30"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-06-11"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+      ],
+      category: [{label: "back end"}, {label: "dev web"}],
+      isDefault: false
+    },
+    {
+      token: 2,
+      name: "MySQL básico",
+      user_list: 10,
+      subject_label: "Lógica de programação",
+      level: 1,
+      schedule_list: [
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-04-20"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-05-30"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-06-11"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+      ],
+      category: [{label: "back end"}, {label: "dev web"}, {label: "dba"}],
+      isDefault: false
+    },
+    {
+      token: 3,
+      name: "Lógica de programação",
+      user_list: 35,
+      subject_label: "Lógica de programação",
+      level: 3,
+      schedule_list: [
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-04-20"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-05-30"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-06-11"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+      ],
+      category: [{label: "back end"}, {label: "dev web"}],
+      isDefault: true
+    },
+    {
+      token: 4,
+      name: "Node para iniciantes",
+      user_list: 15,
+      subject_label: "Lógica de programação",
+      level: 0,
+      schedule_list: [
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-04-20"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-05-30"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-06-11"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+      ],
+      category: [{label: "back end"}, {label: "dev web"}],
+      isDefault: false
+    },
+    {
+      token: 5,
+      name: "MySQL básico",
+      user_list: 10,
+      subject_label: "Lógica de programação",
+      level: 1,
+      schedule_list: [
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-04-20"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-05-30"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-06-11"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+      ],
+      category: [{label: "back end"}, {label: "dev web"}, {label: "dba"}],
+      isDefault: false
+    },
+    {
+      token: 6,
+      name: "Lógica de programação",
+      user_list: 35,
+      subject_label: "Lógica de programação",
+      level: 3,
+      schedule_list: [
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-04-20"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-05-30"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+        {
+          id: Math.random() * 1000,
+          datetime: new Date("2021-06-11"),
+          link: "https://matias.ma/nsfw/",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan congue aliquet. Nam accumsan vestibulum condimentum. Vivamus vulputate placerat tortor accumsan luctus. Maecenas quis iaculis ipsum. Cras semper consectetur diam, sed hendrerit nunc tincidunt ac. Ut pharetra diam arcu.",
+          owner: Math.random() * 1000
+        },
+      ],
+      category: [{label: "back end"}, {label: "dev web"}],
+      isDefault: true
+    }
+  ],
+  interest_list: [
+    {subject_label: "Lógica de programação", level: "", isDefault: true },
+    {subject_label: "Node", level: "", isDefault: false },
+  ]
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- web/src/app/views/group/home-group/home-group.component.ts
+++ web/src/app/views/group/home-group/home-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { userMock } from "src/app/app.component";
 
 @Component({
   selector: "app-home-group",
@@ -9,139 +10,7 @@ import { ActivatedRoute } from "@angular/router";
 export class HomeGroupComponent implements OnInit {
   public a: string = "socorro";
 
-  public user = {
-    name: "José",
-    lastname: "Bezerra",
-    username: "Flynn Rider",
-    email: "jose@email.com.br",
-    level: "Iniciante",
-    group_list: [
-      {
-        token: 1,
-        name: "Node para iniciantes",
-        user_list: 15,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [{ label: "back end" }, { label: "dev web" }],
-        isDefault: false,
-      },
-      {
-        token: 2,
-        name: "MySQL básico",
-        user_list: 10,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [
-          { label: "back end" },
-          { label: "dev web" },
-          { label: "dba" },
-        ],
-        isDefault: false,
-      },
-      {
-        token: 3,
-        name: "Lógica de programação",
-        user_list: 35,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [{ label: "back end" }, { label: "dev web" }],
-        isDefault: true,
-      },
-      {
-        token: 4,
-        name: "Node para iniciantes",
-        user_list: 15,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [{ label: "back end" }, { label: "dev web" }],
-        isDefault: false,
-      },
-      {
-        token: 5,
-        name: "MySQL básico",
-        user_list: 10,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [
-          { label: "back end" },
-          { label: "dev web" },
-          { label: "dba" },
-        ],
-        isDefault: false,
-      },
-      {
-        token: 6,
-        name: "Lógica de programação",
-        user_list: 35,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [{ label: "back end" }, { label: "dev web" }],
-        isDefault: true,
-      },
-      {
-        token: 7,
-        name: "Node para iniciantes",
-        user_list: 15,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [{ label: "back end" }, { label: "dev web" }],
-        isDefault: false,
-      },
-      {
-        token: 8,
-        name: "MySQL básico",
-        user_list: 10,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [
-          { label: "back end" },
-          { label: "dev web" },
-          { label: "dba" },
-        ],
-        isDefault: false,
-      },
-      {
-        token: 9,
-        name: "Lógica de programação",
-        user_list: 35,
-        level: "iniciante",
-        schedule_list: [
-          { datetime: new Date("2021-04-20") },
-          { datetime: new Date("2021-04-28") },
-        ],
-        category: [{ label: "back end" }, { label: "dev web" }],
-        isDefault: true,
-      },
-    ],
-    interest_list: [
-      { subject_label: "Lógica de programação", level: "", isDefault: true },
-      { subject_label: "Node", level: "", isDefault: false },
-    ],
-  };
+  public user = userMock;
 
   constructor(private route: ActivatedRoute) {
   }
