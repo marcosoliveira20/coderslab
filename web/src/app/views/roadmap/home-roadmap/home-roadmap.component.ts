5bbf13d0d49b29d8605168d3d8e957109f50ca93
Marcos Santos <marcos.oliveira129@hotmail.com>
2021-05-11 22:55:06 -0300

    roteamento de roadmap

---
+++ web/src/app/views/roadmap/home-roadmap/home-roadmap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-roadmap',
+  templateUrl: './home-roadmap.component.html',
+  styleUrls: ['./home-roadmap.component.scss']
+})
+export class HomeRoadmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ web/src/app/views/roadmap/new-roadmap/new-roadmap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-roadmap',
+  templateUrl: './new-roadmap.component.html',
+  styleUrls: ['./new-roadmap.component.scss']
+})
+export class NewRoadmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
