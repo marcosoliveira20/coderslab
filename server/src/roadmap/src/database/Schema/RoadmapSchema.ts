5a819889f7dafaa27ad34aea71553a4e56cc8183
Davi Fonseca <davi.brandao2013@gmail.com>
2021-05-21 21:00:58 -0300

    eslint

--- server/src/roadmap/src/database/Schema/RoadmapSchema.ts
+++ server/src/roadmap/src/database/Schema/RoadmapSchema.ts
@@ -1,13 +1,16 @@
 import { Schema, model } from "mongoose";
 
-const RoadmapSchema = new Schema({
-  name: { type: String, required: true, unique: false},
-  is_default: { type: Boolean, required: false, default: true },
-  objective: { type: String, required: true },
-  is_done: { type: Boolean, required: false, default: false },
-  level: {type: String, required: true, default: "0"}
-}, {
-  timestamps: true,
-});
+const RoadmapSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: false },
+    is_default: { type: Boolean, required: false, default: true },
+    objective: { type: String, required: true },
+    is_done: { type: Boolean, required: false, default: false },
+    level: { type: String, required: true, default: "0" },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default model("roadmap", RoadmapSchema);
