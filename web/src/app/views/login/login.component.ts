8ddcdc4f90b07c9aae339caa97e8b46e24327e40
Marcos Santos <marcos.oliveira129@hotmail.com>
2021-05-23 18:20:53 -0300

    alterações em login

--- web/src/app/views/login/login.component.ts
+++ web/src/app/views/login/login.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { FormBuilder, Validators } from "@angular/forms";
-import { BasicAutoCompleterComponent } from "src/app/component/form/input/input.component";
-// import { interestListMock } from "../../../app/app.component";
-import { interestListMock, subjectMock } from "../../../mock";
+import { subjectMock } from "../../../mock";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
@@ -71,7 +69,6 @@ export class LoginComponent implements OnInit {
 
     this.subjectList.splice(subjectIndex, 1);
 
-
     this.interestList.push({
       _id_subject: subjectId,
       _id_user: "",
@@ -83,32 +80,70 @@ export class LoginComponent implements OnInit {
       label: subjectLabel,
       _id_user: "",
       level: Number(this.registerForm.value.level),
-      levelName: this.levelNameStr(Number(this.registerForm.value.level)),
     });
 
-    this.registerForm.controls['level'].setValue("-1");
-    this.registerForm.controls['subjectForm'].setValue("");
-
+    this.registerForm.controls["level"].setValue("-1");
+    this.registerForm.controls["subjectForm"].setValue("");
   }
 
-  updateInterest(subject, value, change) {
+  updateInterest(subject, event, change, index) {
     if (change === "label") {
+      this.interestList.splice(
+        this.interestList.findIndex(
+          (i) => i._id_subject == subject._id_subject
+        ),
+        1
+      );
 
+      this.selectedCustomSubjectList.splice(
+        this.selectedCustomSubjectList.findIndex(
+          (i) => i._id_subject == subject._id_subject
+        ),
+        1
+      );
+
+      const subjectIndex = this.subjectList.findIndex((i) => i.label == event.target.value);
+      const selectedSubjectIndex = this.selectedSubjectList.findIndex(
+        (i) => i.label == subject.label
+      );
+
+      let subjectAdd = this.subjectList[subjectIndex];
+
+      this.selectedSubjectList.push(this.subjectList[subjectIndex]);
+      this.subjectList.splice(subjectIndex, 1);
+
+      this.subjectList.push(this.selectedSubjectList[selectedSubjectIndex]);
+      this.selectedSubjectList.splice(selectedSubjectIndex, 1);
+
+      this.interestList.push({
+        _id_subject: subjectAdd.id,
+        _id_user: "",
+        level: subject.level,
+      });
+
+      this.selectedCustomSubjectList.push({
+        _id_subject: subjectAdd.id,
+        label: event.target.value,
+        _id_user: "",
+        level: subject.level
+      });
+
+      this.selectedCustomSubjectList.sort(function (a, b) {
+        if (a.label < b.label) {
+          return -1;
+        }
+        if (a.label > b.label) {
+          return 1;
+        }
+        return 0;
+      });
     } else {
+      this.selectedCustomSubjectList[index][event.target.name] = event.target.value;
+      this.interestList[index][event.target.name] = event.target.value;
 
     }
   }
 
-  levelNameStr(level: number) {
-    switch (level) {
-      case 0:
-        return "Iniciante";
-      case 1:
-        return "Intermediário";
-      case 2:
-        return "Avançado";
-      default:
-        return "Nivel";
-    }
-  }
+  deleteInterest(subject) {}
+
 }
