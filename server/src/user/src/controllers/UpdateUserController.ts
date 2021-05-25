174abf4a8e5f1df9b16370d0861ff7e44eea6ade
Kesselyn <kesselynfortunato@hotmail.com>
2021-05-24 23:07:11 -0300

    [user] removendo retorno da lista de interesses no update

--- server/src/user/src/controllers/UpdateUserController.ts
+++ server/src/user/src/controllers/UpdateUserController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 
 import { User } from "../model/User";
-import Api from "../../../Api";
 
 class UpdateUserController {
   async handle(request: Request, response: Response) {
@@ -14,8 +13,7 @@ class UpdateUserController {
       email,
       discord_id,
       github_id,
-      password,
-      interest_list
+      password
     } = request.body;
 
     const user = new User();
@@ -37,28 +35,6 @@ class UpdateUserController {
         password,
       });
       
-      const api = new Api();
-
-      try {
-        if(interest_list.length != 0) {
-          for(let i = 0; i < interest_list.length; i++) {
-            let interest;
-
-            if(interest_list[i]._id != "") {
-              interest = await api.interests.put(`/update/${interest_list[i]._id}`, interest_list[i]);
-            } else {
-              interest = await api.interests.post("/create", interest_list[i]);
-            }
-
-            interest_list[i] = interest.data;
-          }
-        }
-
-      } catch(err) {
-        return response.status(err.response.status).send();
-      }
-
-      data.interest_list = interest_list;
       return response.status(200).send(data);
     } catch (err) {
       console.log(err.message);
