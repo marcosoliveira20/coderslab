0e073818f8aefa482fddaeaa0c7cd8376dd76475
Davi Fonseca <davi.brandao2013@gmail.com>
2021-06-02 00:13:38 -0300

    Formatação Date() no deadline

--- server/src/roadmap/src/controllers/CreateRoadmapCustomController.ts
+++ server/src/roadmap/src/controllers/CreateRoadmapCustomController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import Api from "../../../Api";
-import { Roadmap } from "../model/Roadmap";
+import Api from '../../../Api';
+import { Roadmap } from '../model/Roadmap';
 
 class CreateRoadmapCustomController {
   async handle(request: Request, response: Response) {
@@ -12,15 +12,7 @@ class CreateRoadmapCustomController {
     const roadmap = new Roadmap();
     const api = new Api();
 
-<<<<<<< HEAD
     const quantityChallenge = 0;
-    for (let i = 0; i < content_list.length; i++) {
-      // const challenge = content_list[i].challenge.length;
-      // quantityChallenge += challenge;
-    }
-=======
-    let quantityChallenge = 0
->>>>>>> ff7d99ed93a25de0e0b40391fd8a17f66cb2c0ad
 
     try {
       const data = await roadmap.createCustom({
@@ -28,51 +20,33 @@ class CreateRoadmapCustomController {
         objective,
         level,
         quantity_contents: content_list.length,
-        // TODO ver isso
-        quantity_challenges: 2,
+        quantity_challenges: quantityChallenge,
         user_id,
       });
 
       await roadmap.updateQuantityOfContents(data._id, content_list.length);
 
       data.quantity_contents = content_list.length;
-      // TODO ver isso
-      data.quantity_challenges = 2;
+      data.quantity_challenges = quantityChallenge;
 
       if (content_list.length == 0) {
         return response.status(201).send(data);
-<<<<<<< HEAD
       }
       try {
         for (let i = 0; i < content_list.length; i++) {
           content_list[i]._roadmap_id = data._id;
           const new_content_list = await api.content.post(
-            "/create",
-            content_list[i]
+            'create/By/Roadmap/Custom',
+            content_list[i],
           );
           content_list[i] = new_content_list.data;
-=======
-      } else {
-        try {
-          for(let i = 0; i < content_list.length; i++) {
-            content_list[i]._roadmap_id = data._id;
-            const new_content_list = await api.content.post("create/By/Roadmap/Custom", content_list[i]);
-            content_list[i] = new_content_list.data
-          }
-        } catch(err) {
-          await roadmap.delete(data._id)
-          await api.content.delete(`/delete/roadmapid/${data._id}`);
-
-          console.log(err.response.message)
-          return response.status(err.response.status).send();
->>>>>>> ff7d99ed93a25de0e0b40391fd8a17f66cb2c0ad
         }
       } catch (err) {
         await roadmap.delete(data._id);
         await api.content.delete(`/delete/roadmapid/${data._id}`);
 
-        // console.log(err.response.message);
-        return response.status(400).send();
+        console.log(err.response.message);
+        return response.status(err.response.status).send();
       }
       await roadmap.updateContent_list(data._id, content_list);
       data.content_list = content_list;
