76df818618c3d5f529ed8de2f01c5972c1f3eaa5
Kesselyn <kesselynfortunato@hotmail.com>
2021-06-06 21:36:58 -0300

    Tratativa de autocomplete

--- web/src/app/component/form/input/input.component.ts
+++ web/src/app/component/form/input/input.component.ts
@@ -14,10 +14,12 @@ export class BasicAutoCompleterComponent implements OnInit {
 
   private categoryListFilter = [];
   private categoryListSelected = [];
+  public inputValue = "";
 
   ngOnInit(): void {}
 
   filterList(event) { 
+    this.inputValue = event.target.value;
     event.target.value.length > 0
       ? this.categoryListFilter = this.categoryListSelect.filter(category =>
         category.toLowerCase().includes(event.target.value)
